a6f7896e0c6892784bb62eeb8622d2165a44466f
JAYANTH KUMAR NARAYANA <jayanth@root-shc.ntu.edu.sg>
2021-02-11 08:49:17 +0800

    Modified code to include dimensionality reduction using Variational auto-encoders
    Results:
    	Accuracy still low for 3 class classification
    
    Stick to 2-class first and include more features

--- custom_model1.py
+++ custom_model1.py
@@ -109,12 +109,56 @@ y_train=y_train.replace({"NonEx":0,"Exacerbator":1,"FreqEx":2})
 y_test=y_test["ExacerbatorState"]
 y_test=y_test.replace({"NonEx":0,"Exacerbator":1,"FreqEx":2})
 
+
+#Dimension reduction --VAE to 14_dimension
+def vae(X_train,y_train,X_test,dims = [14], epochs=2000, batch_size=1, verbose=2, loss='mse', output_act=False, act='relu', patience=25, beta=1.0, warmup=True, warmup_rate=0.01, val_rate=0.2, no_trn=False,seed=0):
+        # callbacks for each epoch
+        modelName = "vae_model" + '.h5'
+        callbacks = [EarlyStopping(monitor='val_loss', patience=patience, mode='min', verbose=1),
+                     ModelCheckpoint(modelName, monitor='val_loss', mode='min', verbose=1, save_best_only=True,save_weights_only=True)]
+        # warm-up callback
+        warm_up_cb = LambdaCallback(on_epoch_end=lambda epoch, logs: [warm_up(epoch)])  # , print(epoch), print(K.get_value(beta))])
+        # warm-up implementation
+        def warm_up(epoch):
+            val = epoch * warmup_rate
+            if val <= 1.0:
+                K.set_value(beta, val)
+        # add warm-up callback if requested
+        if warmup:
+            beta = K.variable(value=0.0)
+            callbacks.append(warm_up_cb)
+        # spliting the training set into the inner-train and the inner-test set (validation set)
+        X_inner_train, X_inner_test, y_inner_train, y_inner_test = train_test_split(X_train,y_train,
+                                                                                    test_size=val_rate,
+                                                                                    random_state=seed,
+                                                                                    stratify=y_train)
+        # insert input shape into dimension list
+        dims.insert(0, X_inner_train.shape[1])
+        # create vae model
+        vae, encoder, decoder = DNN_models.variational_AE(dims, act=act, recon_loss=loss, output_act=output_act, beta=beta)
+        vae.summary()
+        if no_trn:
+                return
+        # fit
+        history = vae.fit(X_inner_train, epochs=epochs, batch_size=batch_size, callbacks=callbacks, verbose=verbose, validation_data=(X_inner_test, None))
+        # load best model
+        vae.load_weights(modelName)
+        encoder = vae.layers[1]
+        # applying the learned encoder into the whole training and the test set.
+        X_train_m, _, X_train = encoder.predict(X_train)
+        X_test_m, _, X_test = encoder.predict(X_test)
+        return(X_train_m,X_test_m,history)
+
+#Dimensionality reduction
+D_X_train_d,D_X_test_d,history= vae(X_train_d,y_train,X_test_d)
+
+
 #Logistic-regression
 lr = LogisticRegression(random_state=0,penalty="l1",class_weight="balanced",n_jobs=-1)
-lr.fit(X_train_d,y_train)
-print("Training Acc",lr.score(X_train_d,y_train))
-print("Testing Acc",lr.score(X_test_d,y_test))
-y_pred=lr.predict(X_test_d)
+lr.fit(D_X_train_d,y_train)
+print("Training Acc",lr.score(D_X_train_d,y_train))
+print("Testing Acc",lr.score(D_X_test_d,y_test))
+y_pred=lr.predict(D_X_test_d)
 print("Confusion Matrix ",confusion_matrix(y_test,y_pred))
 print("F Score in weighted fashion ",f1_score(y_test,y_pred,average="weighted"))
 
