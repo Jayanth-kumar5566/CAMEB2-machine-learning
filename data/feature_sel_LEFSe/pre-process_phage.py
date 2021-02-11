86b5af9631011212b6adfa40fae8885744288e93
Jayanth Kumar Narayana <jayanth.kumar@tuta.io>
2021-02-11 10:51:06 +0800

    Feature selection added AMR and phage - LEFSe
    To implement ML and check the accuracy

---
+++ data/feature_sel_LEFSe/pre-process_phage.py
@@ -0,0 +1,56 @@
+import pandas
+from skbio.stats.composition import clr
+from scipy.stats import mannwhitneyu
+from scipy.stats import kruskal
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+import pickle 
+
+df=pandas.read_csv("./../../../MASTER-TABLES/VIRFINDER/c10k_abundance_demovir_Family.csv",index_col=0)
+
+
+
+df.drop(["Blank","X13LTBlank","X76LTBlank"],axis=1,inplace=True)
+df.drop(["Unassigned"],axis=0,inplace=True)
+
+df=df.transpose()
+
+
+y1=pandas.read_csv("./../../../METADATA/data_194.csv",index_col=0)
+y2=pandas.read_csv("./../../../METADATA/data_test.csv",index_col=0)
+
+
+#data_normalisation - rel abund
+df_norm=(df.div(df.sum(axis=1),axis=0))*100
+df_norm=df_norm.fillna(0)
+
+#Filtering
+ind=(df_norm>=0)
+f_ind=(ind.sum(axis=0)>=13) #5% of the population 
+df_norm=df_norm.loc[:,f_ind]
+
+df_norm=(df_norm.div(df_norm.sum(axis=1),axis=0))*100
+df_norm=df_norm.fillna(0)
+##Merge Train and test
+
+y=pandas.concat([y1,y2],axis=0)
+
+
+#y=y[(y.ExacerbatorState=="FreqEx") | (y.ExacerbatorState=="NonEx")]
+y=y.replace({"NonEx":0,"Exacerbator":1,"FreqEx":1})
+train=df_norm.reindex(y.index)
+
+train=pandas.merge(train,y["ExacerbatorState"],left_index=True,right_index=True)
+
+#Create a dictionary
+dic_=dict()
+for i in range(len(train.columns)):
+	dic_["S"+str(i)]=train.columns[i]
+
+train.columns=dic_.keys()
+
+file_pi = open('./phage/index_dict_.obj', 'wb') 
+pickle.dump(dic_, file_pi)
+file_pi.close()
+
+train.to_csv("./phage/to_lefse_pathways.csv",sep='\t')
