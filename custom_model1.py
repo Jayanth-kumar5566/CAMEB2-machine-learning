b69e5e2a2602fafcba67cbb651d1eadc1a4b8ff9
JAYANTH KUMAR NARAYANA <jayanth@root-shc.ntu.edu.sg>
2021-02-17 13:02:39 +0800

    Changed the alpha to 0.1, so 90% confidence - resulted in 66 features
    Implemented random forest and tuned it to prevent over-fitting
    
    Max test CV accuracy derived = 63.4%
    
    Feature importance - derived
    
    ---Final analysis using microbes ----- (No Dim reduction)---

--- custom_model1.py
+++ custom_model1.py
@@ -6,11 +6,13 @@ import pandas as pd
 import matplotlib
 import matplotlib.pyplot as plt
 from skbio.stats.composition import clr
+import sys
 
 # importing sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import LeaveOneOut
 from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import RepeatedStratifiedKFold
 from sklearn.decomposition import PCA
 from sklearn.random_projection import GaussianRandomProjection
 from sklearn import cluster
@@ -49,6 +51,9 @@ import importlib
 import DNN_models
 import exception_handle
 
+
+#args
+args=sys.argv
 
 #Reading the data - training
 df=pd.read_csv("./../Data_21Dec20/species_data.csv",index_col=0)
@@ -257,9 +262,9 @@ def vae(X_train,y_train,X_test,dims = [14], epochs=2000, batch_size=1, verbose=2
 D_X_train_d,D_X_test_d,history= vae(X_train_d,y_train,X_test_d,dims=[80,20])
 saveLossProgress(history)
 '''
-#D_X_train_d,D_X_test_d=X_train_d,X_test_d
+D_X_train_d,D_X_test_d=X_train_d,X_test_d
 
-D_X_train_d,D_X_test_d = pca(X_train_d,X_test_d,ratio=0.95)
+#D_X_train_d,D_X_test_d = pca(X_train_d,X_test_d,ratio=0.99)
 
 #Logistic-regression
 def score(x,y=y_train):
@@ -275,82 +280,109 @@ def score(x,y=y_train):
 	balanced_acc=np.mean(table)
 	return(balanced_acc.values[0])
 
+'''
 # define models and parameters
 solvers = ['liblinear']
 penalty = ['l1']
 #c_values = [100, 10, 1.0, 0.1, 0.01,0.001]
-c_values = np.linspace(0.01,100,50)
+c_values = np.linspace(0.01,10,50)
 
 #scoring={"Acc":make_scorer(accuracy_score)}
 scoring={"Acc":make_scorer(balanced_accuracy_score)}
 # define grid search
 grid = dict(solver=solvers,penalty=penalty,C=c_values)
 model = LogisticRegression(fit_intercept=True,class_weight="balanced")
-clf = GridSearchCV(estimator=model, param_grid=grid, cv=LeaveOneOut(), scoring=scoring,n_jobs=-1,return_train_score=True,refit=False)
+if args[1]=="LOOCV":
+    clf = GridSearchCV(estimator=model, param_grid=grid, cv=LeaveOneOut(), scoring=scoring,n_jobs=-1,return_train_score=True,refit=False)
+elif args[1]=="RepeatedKfold":
+    clf = GridSearchCV(estimator=model, param_grid=grid, cv=RepeatedStratifiedKFold(n_splits=5, n_repeats=10), scoring=scoring,n_jobs=-1,return_train_score=True,refit=False)
+    
 clf.fit(D_X_train_d, y_train)
 
 '''
 #Random Forest
-hyper_parameters = [{'n_estimators': [150],'criterion':['gini'],
+hyper_parameters = [{'n_estimators': [10,150],'criterion':['gini'],
                         'max_features': ['auto'],
-                        'max_depth':[s for s in range(4, 10, 1)],
-                        'min_samples_split':[s for s in range(6, 12, 1)],
-                        'min_samples_leaf':[s for s in range(2, 13, 1)],
+                        'max_depth':[None],
+                        'min_samples_split':[i for i in np.linspace(0.001,0.5,50)],
+                        'min_samples_leaf':[i for i in np.linspace(0.001,0.5,50)],
                         }, ]
-scoring={"Acc":make_scorer(accuracy_score)}
+scoring={"Acc":make_scorer(balanced_accuracy_score)}
 
-clf = GridSearchCV(RandomForestClassifier(n_jobs=-1, class_weight="balanced",bootstrap=True), hyper_parameters, cv=LeaveOneOut(), scoring=scoring, n_jobs=-1, verbose=1,refit="Acc",return_train_score=True)
+if args[1]=="LOOCV":
+    clf = GridSearchCV(RandomForestClassifier(n_jobs=-1, class_weight="balanced",bootstrap=True), hyper_parameters, cv=LeaveOneOut(), scoring=scoring, n_jobs=-1, verbose=1,refit="Acc",return_train_score=True)
+elif args[1]=="RepeatedKfold":
+    clf = GridSearchCV(RandomForestClassifier(n_jobs=-1, class_weight="balanced",bootstrap=True), hyper_parameters, cv=RepeatedStratifiedKFold(n_splits=5, n_repeats=10), scoring=scoring, n_jobs=-1, verbose=1,refit=False,return_train_score=True)
 
 clf.fit(X_train_d, y_train)                         
-'''
 
 results = clf.cv_results_
 df=pd.DataFrame(results)
 
-df_test=df.filter(regex=("split.*_test_Acc"))
+if args[1]=="LOOCV":
 
 
-Acuracy_cbalanced=df_test.apply(score,axis=1)
-Acuracy_cbalanced=pd.DataFrame(Acuracy_cbalanced,columns=["Class_average Accuracy"])
-del df_test
+    df_test=df.filter(regex=("split.*_test_Acc"))
 
 
-#===================Training_Acc============================
-train_acc_balanced=df["mean_train_Acc"]
-
-plt.plot(c_values,train_acc_balanced,label="train")
-plt.plot(c_values,Acuracy_cbalanced,label="test_balanced")
-plt.legend()
-plt.title("l1 norm")
-plt.savefig("lr1_plot.png",dpi=600)
+    Acuracy_cbalanced=df_test.apply(score,axis=1)
+    Acuracy_cbalanced=pd.DataFrame(Acuracy_cbalanced,columns=["Class_average Accuracy"])
+    del df_test
 
 
-print("Class balanced Accuracy",Acuracy_cbalanced)
-print("Max Class balanced Accuracy",Acuracy_cbalanced.max())
+    #===================Training_Acc============================
+    train_acc_balanced=df["mean_train_Acc"]
 
-def get_nest(df):
-    return(df["n_estimators"])
+    plt.plot(c_values,train_acc_balanced,label="train")
+    plt.plot(c_values,Acuracy_cbalanced,label="test_balanced")
+    plt.legend()
+    plt.title("l1 norm")
+    plt.savefig("lr1_plot.png",dpi=600)
 
-def get_maxdept(df):
-    return(df["max_depth"])
-    
-def get_min_ss(df):
-    return(df["min_samples_split"])
-    
-def get_min_sl(df):
-    return(df["min_samples_leaf"])    
 
-Acuracy_cbalanced["n_estimators"]=df.loc[:,"params"].apply(get_nest)
-Acuracy_cbalanced["max_depth"]=df.loc[:,"params"].apply(get_maxdept)
-Acuracy_cbalanced["min_samples_split"]=df.loc[:,"params"].apply(get_min_ss)
-Acuracy_cbalanced["min_samples_leaf"]=df.loc[:,"params"].apply(get_min_sl)
-Acuracy_cbalanced["training_Acc"]=train_acc_balanced
+    print("Class balanced Accuracy",Acuracy_cbalanced)
+    print("Max Class balanced Accuracy",Acuracy_cbalanced.max())
 
-Acuracy_cbalanced.to_csv("tuning_res.csv")
+    def get_nest(df):
+        return(df["n_estimators"])
+
+    def get_maxdept(df):
+        return(df["max_depth"])
+        
+    def get_min_ss(df):
+        return(df["min_samples_split"])
+        
+    def get_min_sl(df):
+        return(df["min_samples_leaf"])    
+
+    Acuracy_cbalanced["n_estimators"]=df.loc[:,"params"].apply(get_nest)
+    Acuracy_cbalanced["max_depth"]=df.loc[:,"params"].apply(get_maxdept)
+    Acuracy_cbalanced["min_samples_split"]=df.loc[:,"params"].apply(get_min_ss)
+    Acuracy_cbalanced["min_samples_leaf"]=df.loc[:,"params"].apply(get_min_sl)
+    Acuracy_cbalanced["training_Acc"]=train_acc_balanced
+
+    Acuracy_cbalanced.to_csv("tuning_res.csv")
+
+elif args[1]=="RepeatedKfold":
+    c_values=hyper_parameters[0]['min_samples_split']
+    plt.plot(c_values,df["mean_train_Acc"],label="train")
+    plt.plot(c_values,df["mean_test_Acc"],label="test_balanced")
+    plt.legend()
+    plt.title("l1 norm")
+    plt.savefig("lr1_plot.png",dpi=600)
+
+
+plt.plot(df["mean_train_Acc"],df["mean_test_Acc"],'o',markersize=1)
+plt.xlabel("Mean balanced train Acc")
+plt.ylabel("Mean balanced testn Acc")
+plt.savefig("plot_train_test.png",dpi=600)
+
+df_sel=df.loc[:,["params","mean_train_Acc","mean_test_Acc"]]
+df_sel.to_csv("params_acc.csv")
 
 '''
 
-lr = LogisticRegression(penalty="l1",class_weight="balanced",solver="liblinear",C=2,n_jobs=-1)
+lr = LogisticRegression(penalty="l1",class_weight="balanced",solver="liblinear",C=1,n_jobs=-1)
 lr.fit(D_X_train_d,y_train)
 print("Training Acc",lr.score(D_X_train_d,y_train))
 test_score=lr.score(D_X_test_d,y_test)
@@ -360,13 +392,13 @@ print("Confusion Matrix ",confusion_matrix(y_test,y_pred))
 print("F Score in weighted fashion ",f1_score(y_test,y_pred,average="weighted"))
 
 
-
+'''
 #Input the best parameters and then run
 x=[]
 imp=0
 for i in range(100):
-    rf=RandomForestClassifier(n_jobs=-1, n_estimators=150,min_samples_split=11,max_depth=8,min_samples_leaf
-=12,class_weight="balanced",bootstrap=True)
+    rf=RandomForestClassifier(n_jobs=-1, n_estimators=150,min_samples_split=0.001,max_depth=None,min_samples_leaf
+=0.31669,class_weight="balanced",bootstrap=True)
     rf.fit(X_train_d, y_train)
     imp=imp+rf.feature_importances_
     y_pred=rf.predict(X_test_d)
@@ -374,5 +406,6 @@ for i in range(100):
     x.append(rf.score(X_test_d,y_test))
 
 print("Median of testing acc",np.median(x))
-f_imp=pd.DataFrame([i for i in zip(X_train_d,imp)])
-''' 
+f_imp=pd.DataFrame([i for i in zip(X_train_d,imp)],columns=["Features","Importance"])
+f_imp.to_csv("feature_importances.csv")
+
